fe7d70426b8609e008ae6283cb85c334d1fc6f35
Merlo <107740135+MJulianHub@users.noreply.github.com>
2026-04-19 15:01:33 -0600

    feat: implement CategoriesController with views and routes (#5)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "categories/index"
+  get "categories/show"
+  get "categories/new"
+  get "categories/edit"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
