1ef433fdb85a0527329a0d7ebee0e4e906d66d76
Julian Merlo <merloperezj@gmail.com>
2026-04-20 10:32:52 -0600

    feat: implement ProductsController with CRUD operations and views
    - Added ProductsController with actions for index, show, new, create, edit, update, and destroy.
    - Created associated views for product forms, index, show, edit, and new.
    - Introduced ProductsHelper module.
    - Updated routes to include products resource.
    - Added migration to set default value for active attribute in products.
    - Implemented tests for ProductsController actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "categories#index"
-  resources :categories
+  resources :categories  
+  resources :products
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_17_202055) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_20_162657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_17_202055) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.boolean "active"
+    t.boolean "active", default: true, null: false
     t.bigint "category_id", null: false
     t.string "code"
     t.datetime "created_at", null: false
