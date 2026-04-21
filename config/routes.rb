9f12c8b936773d9e06a9c3847d41d60377bfcd72
Julian Merlo <merloperezj@gmail.com>
2026-04-21 11:12:21 -0600

    feat: implement StocksController with CRUD operations and views
    - Added StocksController with actions for index, new, and create.
    - Created associated views for stock movements, including form and index.
    - Introduced StocksHelper module.
    - Updated routes to include stocks resource.
    - Added tests for StocksController actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :categories
   resources :products
+  resources :stocks, only: [ :index, :new, :create ]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
