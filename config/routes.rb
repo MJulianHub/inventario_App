ed00fa8fd96cd1449d3ae78e89dee37f286b4909
Julian Merlo <merloperezj@gmail.com>
2026-04-30 09:51:59 -0600

    style: update button styles and enhance responsive design
    - Added new styles for .btn-default to improve button appearance.
    - Updated category and product views to use .btn-default for "Ver" and "Editar" buttons.
    - Enhanced responsive design with media queries for better layout on smaller screens.
    - Adjusted currency formatting in product show view to include currency symbol and proper separators.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :products
   resources :stocks, only: [ :index, :new, :create ]
+  match "*patch", to: redirect("categories"), via: :all
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
