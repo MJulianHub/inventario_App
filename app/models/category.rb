23a27efe65108dfe813ae7bd414d16fa504e4160
Julian Merlo <merloperezj@gmail.com>
2026-05-12 11:25:04 -0600

    fix: improve product and stock handling
    - Updated the ProductsController to include a note on eager loading addressing the N+1 query problem.
    - Enhanced the update action to provide user feedback upon successful product updates.
    - Adjusted the StocksController to set the stock cost based on the selected product price and removed the cost parameter from strong parameters.
    - Added a method in the Stock model to label actions in Spanish for better clarity in views.
    - Updated views for categories and products to use `presence` for descriptions, ensuring a consistent display of "Sin descripción" when no description is provided.
    - Refined the stock form to disable the cost field and improved JavaScript for better performance in auto-filling the cost based on product selection.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
-  # Una categoria tiene muchos productos y al ser eliminada una categoria se eliminaran los productos
   has_many :products, dependent: :restrict_with_error
   validates :name, presence: true, uniqueness: true
 end
